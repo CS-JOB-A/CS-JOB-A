19807ed0ac56fac3e502f45a949a927298df852d
YUNA-AHN <yunanash1234@gmail.com>
2024-06-01 02:28:30 +0900

    240531 자바 1일차

--- 유나/JAVA/day01/day01_Scanner.java
+++ 유나/JAVA/day01/day01_Scanner.java
@@ -1,6 +1,8 @@
+package day01;
+
 import java.util.Scanner;
 
-public class day01 {
+public class day01_Scanner {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String inputData = scanner.nextLine();
