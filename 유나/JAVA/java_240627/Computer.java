8ae9dc5f83159cb3ee97cc4ea0e15d631d2a968f
YUNA-AHN <yunanash1234@gmail.com>
2024-06-28 01:13:22 +0900

    0627 자바 유나

---
+++ 유나/JAVA/java_240627/Computer.java
@@ -0,0 +1,10 @@
+package java_240627;
+
+public class Computer extends Calculator{
+    // 메소드 오버라이딩
+    @Override // 컴파일시 저오학히 오버라이딩이 되었는지 체크 (생략가능)
+    public double areaCircle(double r){
+        System.out.println("Computer 객체의 areaCircle 실행");
+        return Math.PI * r * r;
+    }
+}
