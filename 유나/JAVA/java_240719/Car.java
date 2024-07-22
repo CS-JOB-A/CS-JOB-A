5dda84597628007b266f7b0919cc8b315bc00741
YUNA-AHN <yunanash1234@gmail.com>
2024-07-22 18:05:48 +0900

    우나 자바 0719

---
+++ 유나/JAVA/java_240719/Car.java
@@ -0,0 +1,14 @@
+package java_240719;
+
+public class Car {
+    // 필드
+    Tire tire1 =  new HKTire();
+    Tire tire2 = new HKTire();
+
+    // 메소드
+    void run(){
+        // 인터페이스에 선언된 추상메소드 호출
+        tire1.roll();
+        tire2.roll();
+    }
+}
---
+++ 유나/JAVA/java_240719/Driver.java
@@ -0,0 +1,8 @@
+package java_240719;
+
+public class Driver {
+    // 구현 객체가 대입될 수 있도록 매개변수를 인터페이스 타입으로 선언
+    void drive(Vehicle vehicle) {
+        vehicle.run(); // 인터페이스 메소드 호출
+    }
+}
