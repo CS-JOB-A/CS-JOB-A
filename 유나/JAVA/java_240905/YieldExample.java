a639fbb58637591cc1fe01d969b30cfcb1c1a313
YUNA-AHN <yunanash1234@gmail.com>
2024-09-06 03:23:28 +0900

    유나 자바 14

---
+++ 유나/JAVA/java_240905/YieldExample.java
@@ -0,0 +1,20 @@
+package java_240905;
+
+public class YieldExample {
+    public static void main(String[] args) {
+        WorkThread workThreadA = new WorkThread("workThreadA");
+        WorkThread workThreadB = new WorkThread("workThreadB");
+        workThreadA.start();
+        workThreadB.start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {}
+        workThreadA.work = false;
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {}
+        workThreadA.work = true;
+    }
+}
